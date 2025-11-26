164ca53c5f020698938e329e2a157d3c8042e378
daimler <11229630+germanztz@users.noreply.github.com>
2025-11-26 15:26:11 +0100

    docker

---
+++ docker/kubernetes/install_microk8s.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+NAMESPACE=openwebui
+KUBEALIAS="kubectl='microk8s kubectl'"
+
+1_install_microk8s() {
+
+    if snap list | grep -q "microk8s"; then
+        echo "microk8s already installed"
+    else
+        sudo snap install microk8s --classic # --channel=1.34
+        sudo usermod -a -G microk8s $USER
+        mkdir -p ~/.kube
+        chmod 0700 ~/.kube
+
+        alias kubectl='microk8s kubectl'
+        grep -q "$KUBEALIAS" ~/.bash_aliases || echo "$KUBEALIAS" >> ~/.bash_aliases
+
+        sudo microk8s status --wait-ready
+        sudo microk8s enable hostpath-storage
+        sudo microk8s enable dns
+        sudo microk8s enable community
+        sudo microk8s enable ingress
+        sudo microk8s enable registry
+        sudo microk8s enable nvidia
+        
+        # sudo microk8s enable dashboard
+        # sudo microk8s enable dashboard-ingress
+    fi
+
+    microk8s kubectl get pods --all-namespaces
+    # sudo microk8s kubectl describe secret -n kube-system microk8s-dashboard-token | grep token
+    # echo -e "\n\nhttps://kubernetes-dashboard.127.0.0.1.nip.io\n\n"
+    echo "MicroK8s enabled."
+
+}
+
+2_configure_docker_registry() {
+    kubectl get svc -n container-registry
+    sudo sed -i '0,/{/s//{\n    "insecure-registries": \["localhost:32000"\],/' /etc/docker/daemon.json
+    sudo systemctl restart docker
+    echo "Registry configured and Docker daemon restarted."
+}
+
+3_build_and_push_tts_image() {
+    echo "Building and pushing TTS image to MicroK8s registry..."
+    (cd ../openai-edge-tts && docker build -t localhost:32000/daimler/openai-edge-tts:latest .)
+    docker push localhost:32000/daimler/openai-edge-tts:latest
+    echo "TTS image built and pushed successfully."
+    kubectl scale deployment tts-deploy -n $1 --replicas=0
+    kubectl scale deployment tts-deploy -n $1 --replicas=1
+}
+
+4_build_and_push_openwebui_image() {
+    echo "Building and pushing TTS image to MicroK8s registry..."
+    (cd ../../ && docker build -t localhost:32000/daimler/openwebui:latest .)
+    docker push localhost:32000/daimler/openwebui:latest
+    echo "TTS image built and pushed successfully."
+    # kubectl scale deployment tts-deploy -n $1 --replicas=0
+    # kubectl scale deployment tts-deploy -n $1 --replicas=1
+}
+
+5_apply_yaml() {
+    kubectl create ns $1
+    kubectl apply -f . -n=$1
+}
+
+1_install_microk8s
